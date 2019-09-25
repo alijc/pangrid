27de0390756bc7105eb97d3f4950f93e3f98399a
Ali Corbin <ali.corbin@gmail.com>
2019-09-24 20:51:30 -0700

    commented out json stuff

--- lib/pangrid/plugins/xml.rb
+++ lib/pangrid/plugins/xml.rb
@@ -1,4 +1,4 @@
-# Json
+# Xml
 #
 # cell = { x : int, y : int, contents : string }
 #
@@ -9,68 +9,68 @@
 #           down : [string]
 #         }
 
-require 'json'
+require 'rexml/document'
 
 module Pangrid
 
-class Json < Plugin
+class Xml < Plugin
 
-  DESCRIPTION = "Simple JSON format"
+  DESCRIPTION = "Simple XML format"
 
   def write(xw)
     cells = []
-    (0 ... xw.height).each do |y|
-      (0 ... xw.width).each do |x|
-        cell = xw.solution[y][x]
-        s = case cell.solution
-            when :black; '#'
-            when :null; ''
-            when Rebus; cell.solution.inspect
-            else; cell.solution
-            end
-
-        cells.push({ x: x, y: y, contents: s })
-      end
-    end
-
-    h = {
-      rows: xw.height,
-      cols: xw.width,
-      cells: cells,
-      across: xw.across_clues,
-      down: xw.down_clues
-    }
-
-    ::JSON.generate(h)
+#    (0 ... xw.height).each do |y|
+#      (0 ... xw.width).each do |x|
+#        cell = xw.solution[y][x]
+#        s = case cell.solution
+#            when :black; '#'
+#            when :null; ''
+#            when Rebus; cell.solution.inspect
+#            else; cell.solution
+#            end
+#
+#        cells.push({ x: x, y: y, contents: s })
+#      end
+#    end
+#
+#    h = {
+#      rows: xw.height,
+#      cols: xw.width,
+#      cells: cells,
+#      across: xw.across_clues,
+#      down: xw.down_clues
+#    }
+#
+#    ::JSON.generate(h)
   end
 
   def read(data)
-    json = ::JSON.parse(data)
-    xw = XWord.new
-
-    xw.height = json['rows']
-    xw.width = json['cols']
-    xw.solution = Array.new(xw.height) { Array.new(xw.width) }
-    json['cells'].each do |c|
-      cell = Cell.new
-      s = c['contents']
-      cell.solution =
-        case s
-        when ""; :null
-        when "#"; :black
-        else
-          if s.length == 1
-            s
-          else
-            Rebus.new s
-          end
-        end
-      x, y = c['x'], c['y']
-      xw.solution[y][x] = cell
-    end
-    xw.across_clues = json['across']
-    xw.down_clues = json['down']
-    xw
+#    json = ::JSON.parse(data)
+#    xw = XWord.new
+#
+#    xw.height = json['rows']
+#    xw.width = json['cols']
+#    xw.solution = Array.new(xw.height) { Array.new(xw.width) }
+#    json['cells'].each do |c|
+#      cell = Cell.new
+#      s = c['contents']
+#      cell.solution =
+#        case s
+#        when ""; :null
+#        when "#"; :black
+#        else
+#          if s.length == 1
+#            s
+#          else
+#            Rebus.new s
+#          end
+#        end
+#      x, y = c['x'], c['y']
+#      xw.solution[y][x] = cell
+#    end
+#    xw.across_clues = json['across']
+#    xw.down_clues = json['down']
+#    xw
   end
 
 end
